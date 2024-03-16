8b466f3e7658fc17e62b26a382bef77277edefb6
matheo2027 <matheo.piques@epitech.eu>
2024-03-16 10:32:44 +0100

    Add Front

---
+++ jam.js
@@ -0,0 +1,37 @@
+function searchCity() {
+    var userInput = document.getElementById("userCity").value;
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                document.getElementById("result").innerHTML = xhr.responseText;
+            } else {
+                document.getElementById("result").innerHTML = "Erreur lors de la requête : " + xhr.status;
+            }
+        }
+    };
+
+    xhr.open("POST", "search_city.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("userCity=" + userInput);
+}
+
+function filterData(type) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                document.getElementById("result").innerHTML = xhr.responseText;
+            } else {
+                document.getElementById("result").innerHTML = "Erreur lors de la requête : " + xhr.status;
+            }
+        }
+    };
+
+    xhr.open("POST", "filter_data.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("type=" + type);
+}
